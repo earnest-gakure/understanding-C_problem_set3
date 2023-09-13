5fbee1761d6036f8753bcf28a340729f7003c1c4
Earnest <earnestkihara7@gmail.com>
2023-09-13 18:02:55 +0300

    Update Dict.c

--- problem_set_3/The_dictionary/Dict.c
+++ problem_set_3/The_dictionary/Dict.c
@@ -21,6 +21,7 @@ char the_dictionary[MAX_DEFS][MAX_LENGTH] = {
 };
 
 char *dictionary_pointer=the_dictionary[0];
+char **pointer_to_dictionary_pointer = &dictionary_pointer; // Declare and assign the address
 
 
 void load_dictionary() {
@@ -30,9 +31,9 @@ void load_dictionary() {
 char *lookup(char input_word[]) {
     for (int i = 0; i < MAX_DEFS; i++) {
         if (strcmp(input_word, words_list[i]) == 0) {
-            return dictionary_pointer;
+            return *pointer_to_dictionary_pointer; //return the pointed to value i.e dict_pointer which hass address of the_dict[i]
         }
-        dictionary_pointer=the_dictionary[i+1];
+       (* pointer_to_dictionary_pointer)++;  // Increment the pointer to point to the next definition
     }
   
     return NULL; // Word not found
